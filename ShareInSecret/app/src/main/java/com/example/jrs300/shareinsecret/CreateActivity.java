286722e5a82c4b85347cdd89766218a6edcd0754
99jools <jrs300@student.bham.ac.uk>
2014-07-14 12:16:01 +0100

    changed to writing ciphertext to internal storage

--- ShareInSecret/app/src/main/java/com/example/jrs300/shareinsecret/CreateActivity.java
+++ ShareInSecret/app/src/main/java/com/example/jrs300/shareinsecret/CreateActivity.java
@@ -3,15 +3,12 @@ package com.example.jrs300.shareinsecret;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -56,7 +53,9 @@ public class CreateActivity extends Activity {
     /**
      * called whenever the user clicks the Encrypt and Save button
      */
-    public void encryptAndSave(View view) {
+    public void encryptAndSave(View view) throws IOException, NoSuchPaddingException,
+            InvalidAlgorithmParameterException, NoSuchAlgorithmException, BadPaddingException,
+            IllegalBlockSizeException, InvalidKeyException {
 
 
         //get the plaintext from the screen
@@ -67,98 +66,17 @@ public class CreateActivity extends Activity {
         EditText getFilename = (EditText) findViewById(R.id.text_filename);
         saveName = getFilename.getText().toString();
 
+        //get an output stream
+        FileOutputStream fos = openFileOutput(saveName, Context.MODE_PRIVATE);
 
-        try {
-            //get a new FileOutputStream
-            FileOutputStream fos = getNewFos();
-
-            //encrypt text with new key and write to file
-
-            KeyManagement keyUsedToEncrypt = FileCryptor.encryptString(plaintextIn, fos);
-            showToast("File saved");
-            finish();
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        }
-
-
-
+        //encrypt text with new key and write to file
+        KeyManagement keyUsedToEncrypt = FileCryptor.encryptString(plaintextIn, fos);
+        showToast("File saved");
+        finish();
     }
 
-    /**
-     * takes a string of ciphertext and writes to a file
-     * @param cipherText
-     */
-    public FileOutputStream getNewFos(){
-
-        if (isExternalStorageWritable()) {
-
-            try {
-
-                FileOutputStream fos = openFileOutput(saveName, Context.MODE_APPEND);
-
-                String storageState = Environment.getExternalStorageState();
-                if (storageState.equals(Environment.MEDIA_MOUNTED)) {
-
-                    FileOutputStream fos2 = new FileOutputStream(new File(getExternalFilesDir(null),
-                            saveName));
-
-                }
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
-            }
-        }
-        else Log.e("writefile", "external storage is not writeable");
-
-    }
-
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    /* Checks if external storage is available to at least read */
-    public boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static String encrypt (String text)
-    {
-        return text.toUpperCase();
-    }
-
-    public void showToast(String message)
-
-    {
-
+    public void showToast(String message) {
         Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
-
         toast.show();
-
     }
 }
