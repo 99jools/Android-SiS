d993f51033ed2d0f421cbee1586f31269952894a
99jools <jrs300@student.bham.ac.uk>
2014-07-23 16:11:29 +0100

    temp state

---
+++ ShareInSecret/app/src/main/java/GetDbxAcctMgr.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jrs300 on 23/07/14.
+ */
+public class GetDbxAcctMgr {
+    private static final String appKey = "n8sv033pnhqujme";
+    private static final String appSecret = "mlx7499mv5t3tmq";
+
+}
